c810056f137b7fabe3c2f0b50f036ae685faa80f
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-06-15 10:30:51 +0700

    Add project files.

---
+++ StackUsingArray/StackUsingArray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Program {
+private:
+	string stack_array[5];
+	int top;
+
+public:
+	program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "stack Full" << endl;
+			return "";
+		}
+
+		top++;
+		stack_array[top] = element;
+
+		cout << endl;
+	}
+
+
+};
